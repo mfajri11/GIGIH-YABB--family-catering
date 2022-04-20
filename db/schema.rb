b964e50ed19e65f39350fe400fcb4740fb553e12
Muhamad Fajri <muhammad.fajri18218@student.unsika.ac.id>
2022-04-20 06:51:47 +0000

    Migrate all table in schema

---
+++ db/schema.rb
@@ -0,0 +1,68 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2022_04_20_064550) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "phoneNumber"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "menu_item_categories", force: :cascade do |t|
+    t.integer "menuItem_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_menu_item_categories_on_category_id"
+    t.index ["menuItem_id"], name: "index_menu_item_categories_on_menuItem_id"
+  end
+
+  create_table "menu_item_orders", force: :cascade do |t|
+    t.integer "menuItem_id", null: false
+    t.integer "order_id", null: false
+    t.integer "qty"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["menuItem_id"], name: "index_menu_item_orders_on_menuItem_id"
+    t.index ["order_id"], name: "index_menu_item_orders_on_order_id"
+  end
+
+  create_table "menu_items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "customer_id"
+    t.float "total"
+    t.datetime "order_date", precision: 6
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "menu_item_categories", "categories"
+  add_foreign_key "menu_item_categories", "menuItems"
+  add_foreign_key "menu_item_orders", "menuItems"
+  add_foreign_key "menu_item_orders", "orders"
+end
